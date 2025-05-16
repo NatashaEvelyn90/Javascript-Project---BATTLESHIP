73fa09a0b17a74c9c7b395d0781894d830a9b9de
Natasha M. Evelyn <natasha.evelyn@mscoding.org>
2025-05-16 12:10:57 -0500

    updating code
    set up for board game is almost complete. Update js code for setting up random click

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+
+
+let ship = document.querySelector(".squares");
+let correct = document.querySelector(".correct")
+let incorrect = document.querySelector (".incorrect");
+
+let pickSquares = document.querySelector("#clickSq");
+pickSquares.addEventListener("click", playGames);
+
+function playGames () {
+    if (ship){}
+}
