cce458009c88d536c4d24e6626803b6c555115bd
Natasha M. Evelyn <natasha.evelyn@mscoding.org>
2025-05-18 20:51:57 -0500

    updating code
    having a hard time understanding what I am doing wrong with the code. Will re-look again to see what is wrong.

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,43 @@
+const playingGrid = document.querySelectorAll("#clickSq");
+const squares = 9;
+let battleship;
+console.log(squares)
 
-
-let ship = document.querySelector(".squares");
-let correct = document.querySelector(".correct")
-let incorrect = document.querySelector (".incorrect");
-
-let pickSquares = document.querySelector("#clickSq");
-pickSquares.addEventListener("click", playGames);
-
-function playGames () {
-    if (ship){}
+function randomLocation() {
+    return Math.floor(Math.random() * squares);
 }
+
+
+// squares.addEventListener("click", playGames);
+
+// function playGames() {
+// battleship = randomLocation();
+// }
+// playGames();
+// randomLocation();
+
+
+// function squareScramble(array) {
+//     return Math.floor(Math.random() * array.length);
+// }
+// const specialArray = [0,1,2,3,4,5,6,7,8];
+// const specialSquares = squareScramble(specialArray);
+// // console.log(specialSquares);
+
+
+// let pickSquares = document.querySelectorAll("#clickSq");
+// pickSquares.addEventListener("click", playGames);
+
+// function playGames() {
+//     let bFound = document.querySelector(".correct");
+//     let bLost = document.querySelector(".incorrect");
+
+//     for (i = 0; i < pickSquares.length; i++) {
+//         if (specialSquares === bFound) {
+//             pickSquares[i].style.color = "green"
+//         } else if (specialSquares === bLost) {
+//             pickSquares[i].style.color = "red"
+//         }
+//     }
+//  }
+//  playGames();
